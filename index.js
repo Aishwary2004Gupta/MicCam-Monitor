f8ae1daae9c2fa59037b3a7cdea4c310351386b4
Aishwary Gupta <aish042004@gmail.com>
2024-10-12 12:39:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const video = document.querySelector('video');
-navigator.mediaDevices.getUserMedia({})
+
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+    video.srcObject = stream;
+    video.play();
+  })
+  .catch(error => {
+    console.error('Error accessing webcam:', error);
+  });
